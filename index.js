4148c1397c5fb536725df71e6ebdd7d95ef1a65c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-08-26 22:36:03 +0200

    adds styles to script

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+
 const robot = document.getElementById("robot");
 robot.addEventListener("click", hiRobot);
-//const eyeLeft = document.getElementsByClassName("eye-left");
-const eyeLeft = document.getElementById("eye-left")
-const eyeRight = document.getElementById("eye-right")
-eyeLeft.style.backgroundImage = "radial-gradient(grey, black)";
-eyeRight.style.backgroundImage = "radial-gradient(grey, black)";
 
 function hiRobot() {
-    console.log("click")
-    eyeLeft.style.backgroundImage = "radial-gradient(yellow 40%, red)";
-    eyeRight.style.backgroundImage = "radial-gradient(yellow 40%, red)";
+    let isTurnedOn;
+    const eyeLeft = document.getElementById("eye-left")
+    const eyeRight = document.getElementById("eye-right")
+
+    const screen = document.getElementById("screen");
+    screen.style.backgroundImage = "radial-gradient(grey, black)"
+    const message = document.getElementById("message");
+    message.innerText = "";
+    if( message.innerText === "") {
+        console.log("off")
+        eyeLeft.style.backgroundImage = "radial-gradient(grey, black)";
+        console.log(eyeLeft)
+        eyeRight.style.backgroundImage = "radial-gradient(grey, black)";
+        screen.style.backgroundImage = "radial-gradient(grey, black)";
+        message.innerText = "";
+    }
+    if(message.innerText === "") {
+        console.log("on")
+        eyeLeft.style.backgroundImage = "radial-gradient(yellow 40%, red)";
+        eyeRight.style.backgroundImage = "radial-gradient(yellow 40%, red)";
+        screen.style.backgroundImage = "radial-gradient(green, black)";
+        message.innerText = "hello there! My name is MR robot ..";
+    }
 }
