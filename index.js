d1dcdbe2cfb9cf088b1ebbe0f2c112e6c8b9cea2
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-08-27 15:27:19 +0200

    adds fish amimation/btn+fix toogle animation robot

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
-
+//ROBOT
 const robot = document.getElementById("robot");
-robot.addEventListener("click", hiRobot);
+let isOn = false;
+robot.addEventListener("click", () => {
+    if (isOn === true) {robotOff()}
+    else {robotOn()}
+});
 
-function hiRobot() {
+const eyeLeft = document.getElementById("eye-left")
+const eyeRight = document.getElementById("eye-right")
 
-    const eyeLeft = document.getElementById("eye-left")
-    const eyeRight = document.getElementById("eye-right")
+const screen = document.getElementById("screen");
+screen.style.backgroundImage = "radial-gradient(grey, black)"
+const message = document.getElementById("message");
+message.innerText = "";
+const weelOuter = document.getElementById("weel-outer");
 
-    const screen = document.getElementById("screen");
-    screen.style.backgroundImage = "radial-gradient(grey, black)"
-    const message = document.getElementById("message");
+const handLeft = document.querySelector(".hand-left");
+const handRight = document.querySelector(".hand-right");
+
+function robotOff() {
+    eyeLeft.style.backgroundImage = "radial-gradient(grey, black)";
+    eyeRight.style.backgroundImage = "radial-gradient(grey, black)";
+    screen.style.backgroundImage = "radial-gradient(grey, black)";
+    weelOuter.style.transition = "all 1s ease-in-out";
     message.innerText = "";
+    isOn = false;
+}
+function robotOn() {
+    eyeLeft.style.backgroundImage = "radial-gradient(yellow 40%, red)";
+    eyeRight.style.backgroundImage = "radial-gradient(yellow 40%, red)";
+    screen.style.backgroundImage = "radial-gradient(green, black)";
+    weelOuter.style.transform = "rotate(360deg)";
+    weelOuter.style.transition = "all 10s ease-in-out";
+    handRight.style.transform = "rotateY(360deg)";
+    handLeft.style.transform = "rotateY(360deg)";
+    message.innerText = "hello there! My name is MR robot. I would like to be of help, but I'm really just a button ..";
+    isOn = true;
+}
 
-    function robotOff() {
-        eyeLeft.style.backgroundImage = "radial-gradient(grey, black)";
-        console.log(eyeLeft)
-        eyeRight.style.backgroundImage = "radial-gradient(grey, black)";
-        screen.style.backgroundImage = "radial-gradient(grey, black)";
-        message.innerText = "";
-    }
-    function robotOn() {
-        console.log("on")
-        eyeLeft.style.backgroundImage = "radial-gradient(yellow 40%, red)";
-        eyeRight.style.backgroundImage = "radial-gradient(yellow 40%, red)";
-        screen.style.backgroundImage = "radial-gradient(green, black)";
-        return message.innerText = "hello there! My name is MR robot ..";
-    }
-
-    if( message.innerText === "hello there! My name is MR robot ..") {
-        robotOff();
-    }
-    if(message.innerText === "") {
-        robotOn();
-    }
+//FISHBOWL
+const fish = document.getElementById("fish");
+const fishBowl = document.getElementById("fish-bowl");
+let isMoving = false;
+fishBowl.addEventListener("click", () => {
+    if (isMoving === false) {addMotion()}
+    else {stopMotion()}
+})
+function stopMotion() {
+    fish.classList.remove("round-motion");
+    isMoving = false;
+}
+function addMotion() {
+    fish.classList.add("round-motion");
+    isMoving = true;
 }
